f1d2d5246e53c5c427bf27256eb534a976b428b0
Joey Vanhalst <joey.vanhalst@skyline.be>
2023-05-31 10:26:37 +0200

    initial solution

---
+++ Element Info_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Element Info_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("Element Info_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("4F216298-807D-4B88-A13E-ED8135C5329D")]
+[assembly: AssemblyVersion("1.0.0.0")]
